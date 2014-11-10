a7643e497b35d64254391435163f5e92c0bba1ae
LuisSoares <luis.miguel.mendes.soares@gmail.com>
2014-11-09 20:12:21 -0500

    Exercise 1

---
+++ exercise1.py
@@ -0,0 +1,13 @@
+"""First exercise in Coursera Bioinformatics Algorithms(Part1)"""
+
+
+def count(seq,pattern):
+    '''Takes a sequence and a pattern and returns number
+    of occurences of pattern in the sequence including overlapping'''
+    count=0
+    for i in range(0,len(seq)-len(pattern)):
+        if seq[i:i+len(pattern)]==pattern:
+            count+=1
+    return count
+
+
