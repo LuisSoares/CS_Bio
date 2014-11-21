80dd9dd80f9842f273b4d1d80be7dd81ce638b5e
LuisSoares <luis.miguel.mendes.Soares@gmail.com>
2014-11-20 22:03:52 -0500

    Update exercise1.py

--- exercise1.py
+++ exercise1.py
@@ -3,7 +3,8 @@
 
 def count(seq,pattern):
     '''Takes a sequence and a pattern and returns number
-    of occurences of pattern in the sequence including overlapping'''
+    of occurences of pattern in the sequence including overlapping
+    this is importat beCause python string count method doesn't Count overlaps'''
     count=0
     for i in range(0,len(seq)-len(pattern)):
         if seq[i:i+len(pattern)]==pattern:
